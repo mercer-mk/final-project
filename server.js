b1513afb9003c47fb9c887066dfe1aae6c453979
noob-coder-212 <mkhunt2@my.centennialcollege.ca>
2023-12-09 10:52:10 -0500

    changed server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(passport.session());
 app.use(flash());
 
 // Routes
-app.use("/", require("./routes/index"));
+
 app.use("/users", require("./routes/users"));
 app.use("/tasks", require("./routes/tasks"));
 
