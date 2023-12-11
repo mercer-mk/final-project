d7f39eed225a0ae9912e8e86ad129640db5299b2
mercer-mk <milanckhunt@gmail.com>
2023-12-11 15:30:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost/task_management_system", {
 
 // Configure Passport
 require("./config/passport")(passport);
+console.log("pushed for demo")
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
